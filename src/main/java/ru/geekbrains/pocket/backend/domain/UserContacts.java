2108c6bc6f9c94104cff51f0f9304b280323d6a9
zundarik <zundarik@gmail.com>
2019-01-19 01:48:08 +1100

    refactor pull #4 refactor UserContacts

--- src/main/java/ru/geekbrains/pocket/backend/domain/UserContacts.java
+++ src/main/java/ru/geekbrains/pocket/backend/domain/UserContacts.java
@@ -1,27 +1,52 @@
 package ru.geekbrains.pocket.backend.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 @Document(collection = "users.contacts")
 public class UserContacts {
 
     @Id
-    ObjectId id;
+    private ObjectId id;
 
     @Indexed
-    User user_id;
+    @Field(value = "<users:_id>")
+    private User userId;
 
-    User contact;
+    private User contact;
 
-    String byname;
+    @Size(min = 1, max = 50)
+    @Field(value = "byname")
+    private String byName;
 
-    Date added_at;
+    @Field(value = "added_at")
+    private Date addedAt;
 
+    public UserContacts(User userId, User contact, String byName, Date addedAt) {
+        this.userId = userId;
+        this.contact = contact;
+        this.byName = byName;
+        this.addedAt = addedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "UsersContacts{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", contact=" + contact +
+                ", byname=" + byName +
+                ", added_at=" + addedAt +
+                '}';
+    }
 }
