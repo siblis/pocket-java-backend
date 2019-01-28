0b64649555de310236343f96afef7d839ed88e56
nairy71 <odiccey07@gmail.com>
2019-01-28 14:07:18 +0300

    Create ChatWebsocketController and UtilityWebsocketController

---
+++ src/main/java/ru/geekbrains/pocket/backend/controller/websocket/UtilityWebsocketController.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.pocket.backend.controller.websocket;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.geekbrains.pocket.backend.domain.User;
+import ru.geekbrains.pocket.backend.response.GreetingResponse;
+import org.springframework.web.socket.AbstractWebSocketMessage;
+import  org.springframework.web.socket.PingMessage;
+import java.sql.Timestamp;
+
+@Controller
+public class UtilityWebsocketController {
+    public void serverPing(Timestamp timestamp){};
+    public void clientPong(Timestamp timestamp){};
+}
