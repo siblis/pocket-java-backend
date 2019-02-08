e25b2756e720d35ef9b2e77dfd0d70fdbb4f3c9d
nairy71 <odiccey07@gmail.com>
2019-02-08 18:12:28 +0300

    Start create RestContactsController

--- src/main/java/ru/geekbrains/pocket/backend/repository/UserContactsRepository.java
+++ src/main/java/ru/geekbrains/pocket/backend/repository/UserContactsRepository.java
@@ -2,7 +2,15 @@ package ru.geekbrains.pocket.backend.repository;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+import ru.geekbrains.pocket.backend.domain.db.User;
 import ru.geekbrains.pocket.backend.domain.db.UserContacts;
 
+import java.util.List;
+
+@Repository
 public interface UserContactsRepository extends MongoRepository<UserContacts, ObjectId> {
+    List<UserContacts> findByUser(User user);
+
 }
--- src/main/java/ru/geekbrains/pocket/backend/service/UserContactService.java
+++ src/main/java/ru/geekbrains/pocket/backend/service/UserContactService.java
@@ -1,12 +1,15 @@
 package ru.geekbrains.pocket.backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.geekbrains.pocket.backend.domain.db.User;
 import ru.geekbrains.pocket.backend.domain.db.UserContacts;
+import ru.geekbrains.pocket.backend.repository.UserContactsRepository;
 
 import java.util.List;
 
 public interface UserContactService {
-
+    @Autowired
+    public void setUserCobtactsRepoditory();
     public String newUsersContact(UserContacts newContact);
 
     public String deleteUsersContact(UserContacts contactOnDelete);
