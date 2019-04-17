8017eaeaf909af7407ee8c582ba66175135ddda4
zundar <zundarik@gmail.com>
2019-04-17 19:49:32 +1100

    0.1.0-SNAPSHOT without Roles & Privileges http 8888

--- src/main/java/ru/geekbrains/pocket/backend/PocketBackendApplication.java
+++ src/main/java/ru/geekbrains/pocket/backend/PocketBackendApplication.java
@@ -3,7 +3,7 @@ package ru.geekbrains.pocket.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication//(scanBasePackages = "ru.geekbrains.pocket.backend")
+@SpringBootApplication
 public class PocketBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(PocketBackendApplication.class, args);
