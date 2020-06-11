afb48ba29cc50c4e2c3d8b86e332bf38eab9bea7
zundar <zundarik@gmail.com>
2020-06-11 20:28:01 +1100

    v0.1.3 update dependencies & url to db

--- src/main/java/ru/geekbrains/pocket/backend/controller/advice/WebRoleExceptionHandler.java
+++ src/main/java/ru/geekbrains/pocket/backend/controller/advice/WebRoleExceptionHandler.java
@@ -1,19 +1,20 @@
 package ru.geekbrains.pocket.backend.controller.advice;
 
-import org.springframework.hateoas.VndErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 import ru.geekbrains.pocket.backend.exception.RoleNotFoundException;
 
 @ControllerAdvice
 public class WebRoleExceptionHandler {
+
     @ExceptionHandler(RoleNotFoundException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    VndErrors roleNotFoundExceptionHandler(RoleNotFoundException ex) {
-        return new VndErrors("error", ex.getMessage());
+    ResponseStatusException roleNotFoundExceptionHandler(RoleNotFoundException ex) {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 }
